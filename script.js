0f2b2eb78da9f5226f3bc315e0caf29e731bb231
Diogo Teles De Carvalho (DHL Supply Chain) <DiogoTeles.DeC@dhl.com>
2025-10-30 21:10:58 -0300

    Projeto finalizado total

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
-$(document).ready(function() {
-    $("#mobile-btn").on("click", function() {
+$(document).ready(function () {
+    $("#mobile-btn").on("click", function () {
         $("#mobile-menu").toggleClass("active");
         const $icon = $("#mobile-btn i");
         $icon.toggleClass("fa-bars fa-xmark");
     });
+
+    const sections = $("section");
+    const navItens = $(".nav-item");
+
+    $(window).on("scroll", function () {
+        const header = $("header");
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css("box-shadow", "none");
+        }
+        else {
+            header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.1)");
+        }
+
+        sections.each(function (i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+
+        navItens.removeClass("active");
+        $(navItens[activeSectionIndex]).addClass("active");
+    });
+
+    ScrollReveal().reveal("#cta", {
+        origin: "left",
+        duration: 2000,
+        distance: "20%",
+    });
+
+    ScrollReveal().reveal(".dish", {
+        origin: "left",
+        duration: 2000,
+        distance: "20%",
+    });
+
+    ScrollReveal().reveal("#testimonials-chef", {
+        origin: "left",
+        duration: 2000,
+        distance: "20%",
+    });
+
+    ScrollReveal().reveal(".feedbeck", {
+        origin: "right",
+        duration: 2000,
+        distance: "20%",
+    });
+
 });
